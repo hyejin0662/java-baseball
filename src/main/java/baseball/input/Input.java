606c6ffa58b9a02e9fa5d36a89a4e17eb0660a5d
hyejin0662 <hyejin0776@gmail.com>
2022-11-08 20:31:26 +0900

    feat) add Input

---
+++ src/main/java/baseball/input/Input.java
@@ -0,0 +1,9 @@
+package baseball.input;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class Input {
+    public static String inputValue() {
+        return readLine();
+    }
+}
